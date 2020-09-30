2ab3ac82ea510b9e9bae7005aef4d7b75bf4b6a9
Alexey Lazarev <flome777@gmail.com>
2020-09-30 20:19:41 +0500

    Changes for folders and logistic

--- 2/HomeWork.java
+++ 2/HomeWork.java
@@ -3,18 +3,24 @@ import java.util.Arrays;
 
 class HomeWork {
 	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("How many numbers do you need to add?");
+
 		int n = scanner.nextInt();
 		int array[] = new int[n];
 		int arraySum = 0;
 
 		for (int i = 0; i < array.length; i++) {
+			System.out.print("Please, enter the number: ");
 			array[i] = scanner.nextInt();
-			arraySum = n
 		}
 		
+		for (int i =0; i < array.length; i++ ) {
+			arraySum += array[i];
+		}
+
 		System.out.println(Arrays.toString(array));
-		
 		System.out.println(arraySum);
 	}
 }
